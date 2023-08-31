83d43d22f7d9fd72b4404fda2413b3b045018b39
Israel Blanc <iiblanc@CLFIFTECHC02FC1GSMD6N.local>
2023-08-31 18:02:06 -0400

    refactor

--- src/02-component-patters/components/ProductCard.tsx
+++ src/02-component-patters/components/ProductCard.tsx
@@ -11,23 +11,52 @@ interface Product {
     title: string;
     img?: string;
 }
+
+export const ProductImage = ({ img = ''}) => {
+        return (
+            <img className={styles.productImg}  src={img} alt='Product'/>
+        )
+}
+
+export const ProductTitle = ({ title = ''} : {title: string}) => {
+    return (
+        <span className={styles.productDescription}>{title}</span>
+    )
+}
+
+interface ProductButtonsProps {
+    increaseBy: (value: number) => void;
+    counter: number;
+}
+
+
+export const ProductButtons = ({ counter, increaseBy }: ProductButtonsProps) => {
+    return (
+        <div className={styles.buttonsContainer}>
+        <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>
+            -
+        </button>
+        <div className={styles.countLabel}> {counter} </div>
+
+        <button className={styles.buttonAdd} onClick={() => increaseBy(+1)}> + </button>
+</div>
+    )
+}
+
+
 export const ProductCard = ({product}: Props) => {
 
        const {counter, increaseBy } =  useProduct();
+       console.warn(product)
     return (
         <div className={styles.productCard}>
-            <img className={styles.productImg}  src='./coffee-mug.png' alt='Coffe Mug'/>
+           <img className={styles.productImg}  src={product.img} alt='Product'/>
+        <ProductImage img={product.img}/>
+            
+        <ProductTitle title={product.title} />
 
-            <span className={styles.productDescription}>{product.title}</span>
+        <ProductButtons increaseBy={increaseBy}  counter={counter} />
 
-            <div className={styles.buttonsContainer}>
-                    <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>
-                        -
-                    </button>
-                    <div className={styles.countLabel}> {counter} </div>
-
-                    <button className={styles.buttonAdd} onClick={() => increaseBy(+1)}> + </button>
-            </div>
         </div>
     )
 }
