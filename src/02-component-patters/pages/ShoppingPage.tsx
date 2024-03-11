25af5c25f3040638f51d170df2a4cd8c0fa1e6ed
Israel Blanc <iiblanc@CLFIFTECHC02FC1GSMD6N.local>
2024-03-11 15:21:39 -0300

    finish

--- src/02-component-patters/pages/ShoppingPage.tsx
+++ src/02-component-patters/pages/ShoppingPage.tsx
@@ -25,13 +25,19 @@ export const ShoppingPage = () => {
 
                     }}>
                         {
-                            ({reset}) => (
+                            ({reset, count, increaseBy, isMaxCountReached}) => (
                                 <>
                                  <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }} />
                                 <ProductTitle className="text-bold" />
                                 <ProductButtons className="custom-buttons" />
 
                                 <button onClick={reset}> Reset</button>
+                                <button  onClick={() => increaseBy(-2)}> -2 </button>
+                                {
+                                    (!isMaxCountReached && <button onClick={() => increaseBy(2)}> +2 </button>)
+                                }
+                              
+                                <span>{count}</span>
                                 </>
 
                             )
