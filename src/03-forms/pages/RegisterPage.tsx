af845cb090119390ef1bb150e6c2898b39ce134c
Israel Blanc <iiblanc@CLFIFTECHC02FC1GSMD6N.local>
2024-03-13 17:38:38 -0300

    simple form with useState hook

--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -1,18 +1,36 @@
+import { ChangeEvent, useState } from 'react';
 import '../styles/styles.css';
 
 export const RegisterPage = () => {
+
+    const [ registerData, setRegisterData ] = useState({
+        name: '',
+        email: '',
+        password1: '',
+        password2: ''
+    });
+
+    const {name, email, password1, password2 } = registerData;
+
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setRegisterData( prev => ({
+            ...prev,
+            [event.target.name]: event.target.value
+        }))
+    }
+    
     return (
         <div>
             <h1>Register Page</h1>
 
-            <form>
-                <input type="text" placeholder="Name">
+            <form noValidate>
+                <input type="text" placeholder="Name" value={name} onChange={(e) => onChange(e)} name='name'>
                 </input>
-                <input type="email" placeholder="Email">
+                <input type="email" placeholder="Email" value={email} name='email' onChange={(e) => onChange(e)}>
                 </input>
-                <input type="password" placeholder="Password">
+                <input type="password" placeholder="Password" value={password1} name='password1' onChange={(e) => onChange(e)}>
                 </input>
-                <input type="password" placeholder="Repeat Password">
+                <input type="password" placeholder="Repeat Password" value={password2} name='password2' onChange={(e) => onChange(e)}>
                 </input>
 
                 <button type="submit">Create</button>
