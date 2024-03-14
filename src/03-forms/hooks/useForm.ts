70a4fa9b75846f15021120d2baa84f6857a123bb
Israel Blanc <iiblanc@CLFIFTECHC02FC1GSMD6N.local>
2024-03-14 17:41:29 -0300

    1st useForm hook and validations

--- src/03-forms/hooks/useForm.ts
+++ src/03-forms/hooks/useForm.ts
@@ -11,10 +11,20 @@ export const useForm = <T>(initiState: T) => {
         }))
     }
 
+    const resetForm = () => {
+        setFormData({ ...initiState })
+    }
+
+    const isValidEmail = ( email: string ) => {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
     return {
         ...formData,
         formData,
-        onChange
-
+        onChange,
+        resetForm,
+        isValidEmail
     }
 }
--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -4,7 +4,7 @@ import { useForm } from '../hooks/useForm';
 
 export const RegisterPage = () => {
 
-    const  { formData, onChange, name, email, password1, password2 } = useForm({
+    const  { formData, onChange, name, email, password1, password2, isValidEmail } = useForm({
         name: '',
         email: '',
         password1: '',
@@ -21,15 +21,41 @@ export const RegisterPage = () => {
             <h1>Register Page</h1>
 
             <form noValidate onSubmit={(e) => onSubmit(e)}>
-                <input type="text" placeholder="Name" value={name} onChange={(e) => onChange(e)} name='name'>
-                </input>
-                <input type="email" placeholder="Email" value={email} name='email' onChange={(e) => onChange(e)}>
-                </input>
-                <input type="password" placeholder="Password" value={password1} name='password1' onChange={(e) => onChange(e)}>
-                </input>
-                <input type="password" placeholder="Repeat Password" value={password2} name='password2' onChange={(e) => onChange(e)}>
+
+                <input 
+                    type="text" 
+                    placeholder="Name" 
+                    value={name} 
+                    onChange={(e) => onChange(e)} 
+                    name='name'
+                    className={`${name.trim().length <= 0 && 'has-error'}`}
+                    >
                 </input>
 
+                { name.trim().length <= 0 && <span>Este campo es necesario</span> }
+
+                <input 
+                    type="email" 
+                    placeholder="Email" 
+                    value={email} 
+                    name='email' 
+                    onChange={(e) => onChange(e)}
+                    className={`${!isValidEmail(email) && 'has-error'}`}
+                    >
+                </input>
+
+                { !isValidEmail(email) && <span>Email no es valido</span> }
+
+                <input type="password" placeholder="Password" value={password1} name='password1' onChange={(e) => onChange(e)}>
+                </input>
+
+                { password1.trim().length <= 0 && <span>Este Campo no es necesario</span> }
+                { password1.trim().length < 6 && password1.trim().length > 0 && <span>La contraseña tiene que tener 6 caracteres </span> }
+                <input type="password" placeholder="Repeat Password" value={password2} name='password2' onChange={(e) => onChange(e)}>
+                </input>
+                { password2.trim().length <= 0 && <span>Este Campo no es necesario</span> }
+                { password2.trim().length && password1 !== password2 && <span>Las contraseñas tienen que ser iguales </span> }
+                { password2.trim().length < 6 && password1.trim().length > 0 && <span>La contraseña tiene que tener 6 caracteres </span> }
                 <button type="submit">Create</button>
             </form>
         </div>
